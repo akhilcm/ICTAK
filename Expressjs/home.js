c80cd825207dd6be7c306fbcd284b166e0b9495c
akhilcm <akhilcm777@gmail.com>
2019-08-19 19:58:44 +0530

    Update

--- Expressjs/home.js
+++ Expressjs/home.js
@@ -1,12 +1,25 @@
 const Express = require('express');
+var bodyParser= require('body-parser');
 var app=new Express();
 app.set('view engine','ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.get('/',(req,res)=>{
     res.render('home',{'names':["Akhil","Amal","Achu","Appu"]});
 });
 
 app.get('/contact',(req,res)=>{
     res.render('contact');
+});
+
+app.post('/read',(req,res)=>{
+    var body = req.body;
+    // res.send(body);
+    res.render('read',{bod:body});
+
+
+
 });
 
 app.listen(3002);
